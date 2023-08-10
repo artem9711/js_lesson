4879854c359bd0952e2f3b7096ddeae4b7d4325e
Dudochkin Artem <artjob93@gmail.com>
2023-08-10 17:44:16 +0500

    feat(components): import компонента в компонент

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import SinglePostComponent from "@/components/SinglePostComponent.vue";
 </script>
 
 <template>
-
+<single-post-component></single-post-component>
 </template>
 
 <style scoped>
---
+++ resources/js/components/SinglePostComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+    Toch me
+</div>
+</template>
+
+<style scoped>
+
+</style>
