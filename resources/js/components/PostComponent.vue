08224fb9e7667cbf06f031ab039dbf0064601a90
Dudochkin Artem <artjob93@gmail.com>
2023-08-24 08:06:03 +0500

    feat(PostComponent): data, methods, computed

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,9 +1,42 @@
-<script setup>
-import SinglePostComponent from "@/components/SinglePostComponent.vue";
+<script>
+import SinglePostComponent from "./SinglePostComponent.vue";
+
+export default {
+
+    data: function() {
+        return {
+            name: 'Вася',
+            age: 20,
+        }
+    },
+
+    components: {
+        SinglePostComponent
+    },
+
+    methods:{
+        sayHello(){
+            console.log('Hello')
+        },
+        sayHi(){
+            console.log('Hi')
+        }
+    },
+
+    computed: {
+        vasyajob() {
+           return this.name + ' работает в булочной'
+        }
+    }
+}
 </script>
 
 <template>
 <single-post-component></single-post-component>
+    <div>{{ name }}</div>
+    <div>{{ vasyajob }}</div>
+    <button @click="sayHello">Hello</button>
+    <button @click="sayHi">Hi</button>
 </template>
 
 <style scoped>
