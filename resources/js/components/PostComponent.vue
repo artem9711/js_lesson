36c106b055b76b27470373016715271ee6af1a10
Dudochkin Artem <artjob93@gmail.com>
2023-08-24 08:20:38 +0500

    feat(PostComponent): Директива v-for во vuejs

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -3,10 +3,29 @@ import SinglePostComponent from "./SinglePostComponent.vue";
 
 export default {
 
-    data: function() {
+    data: function () {
         return {
-            name: 'Вася',
-            age: 20,
+            persons: [
+                {
+                    id:1,
+                    name: 'Вася',
+                    age: 20,
+                    job: 'coach'
+                },
+                {
+                    id:2,
+                    name: 'Юлия',
+                    age: 29,
+                    job: 'teacher'
+                },
+                {
+                    id:3,
+                    name: 'Сергей',
+                    age: 26,
+                    job: 'doc'
+                }
+            ]
+
         }
     },
 
@@ -14,29 +33,44 @@ export default {
         SinglePostComponent
     },
 
-    methods:{
-        sayHello(){
+    methods: {
+        sayHello() {
             console.log('Hello')
         },
-        sayHi(){
+        sayHi() {
             console.log('Hi')
         }
     },
 
     computed: {
         vasyajob() {
-           return this.name + ' работает в булочной'
+            return this.name + ' работает в булочной'
         }
     }
 }
 </script>
 
 <template>
-<single-post-component></single-post-component>
-    <div>{{ name }}</div>
-    <div>{{ vasyajob }}</div>
-    <button @click="sayHello">Hello</button>
-    <button @click="sayHi">Hi</button>
+    <single-post-component></single-post-component>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">Job</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="person in persons">
+            <th scope="row">{{ person.id }}</th>
+            <td>{{ person.name }}</td>
+            <td>{{ person.age }}</td>
+            <td>{{ person.job }}</td>
+        </tr>
+
+        </tbody>
+    </table>
 </template>
 
 <style scoped>
